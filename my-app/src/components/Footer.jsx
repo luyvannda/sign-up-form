b3a1733cda5009921b8c41ea30924a32528f25ec
luyvannda <philips.joe329@gmail.com>
2023-10-29 16:29:20 +0700

    set up working folder and add Footer component

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="#">Luy Vannda</a>.
+      </p>
+    </footer>
+  )
+}
