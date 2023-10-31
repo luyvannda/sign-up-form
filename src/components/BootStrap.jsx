89e2bd1bae8363b028a2f4d5198701dcd2e77e8b
luyvannda <philips.joe329@gmail.com>
2023-10-31 10:00:49 +0700

    learn bootstrap grid column

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -6,11 +6,11 @@ export default function Bootstrap() {
         <button className="btn btn-danger">Primary button</button>
       </div>
 
-      <div className="container-sm border">.container-sm</div>
-      <div className="container-md border ">.container-md</div>
-      <div className="container-lg border ">.container-lg</div>
-      <div className="container-xl border">.container-xl</div>
-      <div className="container-xxl border">.container-xxl</div>
+      <div className="row mt-3 mb-3">
+        <div className="col-sm-4 bg-black text-white text-center">Column 1</div>
+        <div className="col-sm-4 bg-primary text-white text-center">Column-2</div>
+        <div className="col-sm-4 bg-danger text-white text-center">Column-3</div>
+      </div>
     </>
   )
 }
