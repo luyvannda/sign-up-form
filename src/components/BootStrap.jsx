13b773f47697ba0030463ab6386af72ae09fb583
luyvannda <philips.joe329@gmail.com>
2023-10-31 10:53:08 +0700

    continue learning bootstrap

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -8,8 +8,16 @@ export default function Bootstrap() {
 
       <div className="row mt-3 mb-3">
         <div className="col-sm-4 bg-black text-white text-center">Column 1</div>
-        <div className="col-sm-4 bg-primary text-white text-center">Column-2</div>
-        <div className="col-sm-4 bg-danger text-white text-center">Column-3</div>
+        <div className="col-sm-8 bg-primary text-white text-center">Column-2</div>
+      </div>
+
+      <div className="container mt-3">
+        <h1>Blockquotes</h1>
+        <p>The blockquote element is used to present content from another source:</p>
+        <blockquote className="blockquote">
+          <p>For 50 years, <abbr title="World Wide Fund for Nature">WWF</abbr>  has been protecting the future of nature. The world&apos; leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
+          <footer className="blockquote-footer">From WWF&apos; website</footer>
+        </blockquote>
       </div>
     </>
   )
