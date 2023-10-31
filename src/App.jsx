ee37b1f03ca0d7481b88f03f36b1ce66cbc9562f
luyvannda <philips.joe329@gmail.com>
2023-10-31 09:14:29 +0700

    practice bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
+import Bootstrap from './components/BootStrap'
 import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <div className="container py-4 px-3 mx-auto">
-        <h1>Hello, Bootstrap and Vite!</h1>
-        <button className="btn btn-danger">Primary button</button>
-      </div>
-
+      <Bootstrap />
       <Footer />
     </>
   )
---
+++ src/components/BootStrap.jsx
@@ -0,0 +1,16 @@
+export default function Bootstrap() {
+  return (
+    <>
+      <div className="container pt-5">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button className="btn btn-danger">Primary button</button>
+      </div>
+
+      <div className="container-sm border">.container-sm</div>
+      <div className="container-md border ">.container-md</div>
+      <div className="container-lg border ">.container-lg</div>
+      <div className="container-xl border">.container-xl</div>
+      <div className="container-xxl border">.container-xxl</div>
+    </>
+  )
+}
