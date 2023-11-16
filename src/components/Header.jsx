1d87119af59b1988d863ebbdbc415090769c2063
luyvannda <philips.joe329@gmail.com>
2023-11-16 11:07:55 +0700

    continue to style the Header component with bootstrap and custom sass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 export default function Header() {
   return (
-    <>
-      <div id="header-container" className="container-sm">
-        <h3>Learn to code by watching others</h3>
-        <p> See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
-      </div>
-    </>
+    <div id="header-container" className="container mb-5 text-center text-white">
+      <h3 className="fw-bold mb-3">Learn to code by <br />watching others</h3>
+      <p> See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable. </p>
+    </div>
   )
 }
