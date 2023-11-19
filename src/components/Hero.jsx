c4eeebce677443179e99cb46e3e8759aaae0db17
luyvannda <philips.joe329@gmail.com>
2023-11-19 10:27:46 +0700

    complete the look of mobile version for the project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="container mb-3 d-grid col-11 mx-auto">
+    <div className="container mb-3 d-grid col-11">
 
       <button id="hero-btn" type="button"
         className="btn text-white py-2 fw-bold ">Try it free 7 days <span className="fw-light">then <br /> $20/mo. thereafter</span></button>
