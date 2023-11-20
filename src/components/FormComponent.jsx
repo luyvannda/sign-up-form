9b0885dec990df09da94c6f0476d8eb1399bf7d7
luyvannda <philips.joe329@gmail.com>
2023-11-20 16:57:00 +0700

    style the form component again

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -3,7 +3,6 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
 
 function FormComponent() {
   const [validated, setValidated] = useState(false);
@@ -19,84 +18,73 @@ function FormComponent() {
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="11" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+    <div id="form-container" className="container col-11 mb-3 d-grid rounded-3">
+      <Form className="rounded-3 mx-3" noValidate validated={validated} onSubmit={handleSubmit}>
+
+        <Form.Group as={Col} md="11" controlId="firstName">
+          <Form.Label></Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="First name"
-            defaultValue="Mark"
+            placeholder="First Name"
           />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type='invalid'>First Name cannot be empty</Form.Control.Feedback>
+
         </Form.Group>
-        <Form.Group as={Col} md="11" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+        <Form.Group as={Col} md="11" controlId="lastName">
+          <Form.Label></Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
+            placeholder="Last Name"
           />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type='invalid'>Last Name cannot be empty</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group as={Col} md="11" controlId="validationCustomUsername">
-          <Form.Label>Email</Form.Label>
+        <Form.Group as={Col} md="11" controlId="email">
+          <Form.Label></Form.Label>
           <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
             <Form.Control
               type="email"
               placeholder="Email"
-              aria-describedby="inputGroupPrepend"
               required
             />
             <Form.Control.Feedback type="invalid">
-              Please enter a valid email.
+              Looks like this is not an email
             </Form.Control.Feedback>
           </InputGroup>
         </Form.Group>
 
-        <Form.Group as={Col} md="11" controlId="validationCustom03">
-          <Form.Label>Password</Form.Label>
+        <Form.Group as={Col} md="11" controlId="password">
+          <Form.Label></Form.Label>
           <Form.Control
             required
             type="password"
             placeholder="Password"
-            defaultValue="password"
           />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type='invalid'>Password cannot be empty</Form.Control.Feedback>
         </Form.Group>
 
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group>
-      <Button type="submit">Submit form</Button>
-    </Form>
+        <div className="d-grid mt-4 mb-3">
+          <Button id='form-btn' type="submit">Claim your free trial</Button>
+        </div>
+      </Form>
+
+      <div className="text-center">
+        <p id="term-text">By clicking the button, you are agreeing to our <a className="text-danger fw-bold" href="https://www.linkedin.com/in/luyvannda/" target="_blank" rel="noreferrer">Terms and Services</a>
+        </p>
+      </div>
+
+    </div>
   );
 }
+
+
+
 
 export default FormComponent;
 
 
-
-
-// import { useState, useRef } from "react"
-// import { Form } from "react-bootstrap";
-
-// export default function FormComponent() {
-//   const [submitted, setSubmitted] = useState(false);
-//   const firstName = useRef(null);
-//   const lastName = useRef(null);
-//   const email = useRef(null);
-//   const password = useRef(null);
 
 //   return (
 //     <div id="form-container" className="container col-11 mb-3 d-grid rounded-3">
