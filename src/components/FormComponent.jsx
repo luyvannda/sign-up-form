516c3b6f5244851a373c632991e0036b634d53d3
luyvannda <philips.joe329@gmail.com>
2023-11-21 10:06:19 +0700

    complete design aspect of the project

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -19,29 +19,34 @@ function FormComponent() {
 
   return (
     <div id="form-container" className="container col-11 mb-3 d-grid rounded-3">
-      <Form className="rounded-3 mx-3" noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form className="rounded-3 mx-2 mx-md-3 mx-xl-4" noValidate validated={validated} onSubmit={handleSubmit}>
 
-        <Form.Group as={Col} md="11" controlId="firstName">
+        <Form.Group as={Col} md="12" controlId="firstName">
           <Form.Label></Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="First Name"
           />
-          <Form.Control.Feedback type='invalid'>First Name cannot be empty</Form.Control.Feedback>
+          <Form.Control.Feedback className='text-end' type='invalid'>First Name cannot be empty</Form.Control.Feedback>
+
+          <Form.Control.Feedback className='text-end' type='valid'>Look good</Form.Control.Feedback>
 
         </Form.Group>
-        <Form.Group as={Col} md="11" controlId="lastName">
+        <Form.Group as={Col} md="12" controlId="lastName">
           <Form.Label></Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Last Name"
           />
-          <Form.Control.Feedback type='invalid'>Last Name cannot be empty</Form.Control.Feedback>
+          <Form.Control.Feedback className='text-end' type='invalid'>Last Name cannot be empty</Form.Control.Feedback>
+
+          <Form.Control.Feedback className='text-end' type='valid'>Look good</Form.Control.Feedback>
+
         </Form.Group>
 
-        <Form.Group as={Col} md="11" controlId="email">
+        <Form.Group as={Col} md="12" controlId="email">
           <Form.Label></Form.Label>
           <InputGroup hasValidation>
             <Form.Control
@@ -49,31 +54,32 @@ function FormComponent() {
               placeholder="Email"
               required
             />
-            <Form.Control.Feedback type="invalid">
+            <Form.Control.Feedback className='text-end' type="invalid">
               Looks like this is not an email
             </Form.Control.Feedback>
+
           </InputGroup>
         </Form.Group>
 
-        <Form.Group as={Col} md="11" controlId="password">
+        <Form.Group as={Col} md="12" controlId="password">
           <Form.Label></Form.Label>
           <Form.Control
             required
             type="password"
             placeholder="Password"
           />
-          <Form.Control.Feedback type='invalid'>Password cannot be empty</Form.Control.Feedback>
+          <Form.Control.Feedback className='text-end' type='invalid'>Password cannot be empty</Form.Control.Feedback>
         </Form.Group>
 
         <div className="d-grid mt-4 mb-3">
           <Button id='form-btn' type="submit">Claim your free trial</Button>
         </div>
-      </Form>
 
-      <div className="text-center">
-        <p id="term-text">By clicking the button, you are agreeing to our <a className="text-danger fw-bold" href="https://www.linkedin.com/in/luyvannda/" target="_blank" rel="noreferrer">Terms and Services</a>
-        </p>
-      </div>
+        <div className="text-center">
+          <p id="term-text">By clicking the button, you are agreeing to our <a className="text-danger fw-bold" href="https://www.linkedin.com/in/luyvannda/" target="_blank" rel="noreferrer">Terms and Services</a>
+          </p>
+        </div>
+      </Form>
 
     </div>
   );
